1ae0d19eea54be1722f6ceffe823ed4841cac85e
nikom1337 <kornel.rabczak@gmail.com>
2019-12-15 13:41:06 +0100

    Implementation for transfer money domain logic

--- src/main/scala/com/thecookiezen/domain/Bank.scala
+++ src/main/scala/com/thecookiezen/domain/Bank.scala
@@ -1,29 +1,38 @@
 package com.thecookiezen.domain
 
-import com.thecookiezen.domain.Balance.Amount
-
 object Bank {
   type Country = String
   type City = String
 
-  case class User(firstName: String, lastName: String, age: Int, address: Address)
-  case class Address(city: City, country: Country, streetName: String, streetNo: Int)
+  case class User(firstName: String,
+                  lastName: String,
+                  age: Int,
+                  address: Address)
+  case class Address(city: City,
+                     country: Country,
+                     streetName: String,
+                     streetNo: Int)
 
   type Error = String
   type ErrorOrAccount = Either[String, Account]
+  type ErrorOr[A] = Either[String, A]
   type Deposit = Account => Balance => ErrorOrAccount
   type Withdraw = Account => Balance => ErrorOrAccount
 
-  type Transfer = Amount => Withdraw => Deposit
+  type Transfer = Balance => Withdraw => Deposit => ErrorOr[(Account, Account)]
 
-  val withdraw: Withdraw = account => balance => {
-    if (account.balance.amount.compare(balance.amount) >= 0)
-      Right(account.copy(balance = Balance.subtract(account.balance, balance)))
-    else
-      Left[Error, Account](s"Account balance ${account.balance} is not sufficient for $balance withdraw")
+  val withdraw: Withdraw = account =>
+    balance => {
+      if (account.balance.amount.compare(balance.amount) >= 0)
+        Right(
+          account.copy(balance = Balance.subtract(account.balance, balance)))
+      else
+        Left[Error, Account](
+          s"Account balance ${account.balance} is not sufficient for $balance withdraw")
   }
 
-  val deposit: Deposit = account => balance => {
-    Right(account.copy(balance = Balance.add(account.balance, balance)))
+  val deposit: Deposit = account =>
+    balance => {
+      Right(account.copy(balance = Balance.add(account.balance, balance)))
   }
 }
--- src/test/scala/com/thecookiezen/domain/BankTransfer.scala
+++ src/test/scala/com/thecookiezen/domain/BankTransfer.scala
@@ -1,30 +1,56 @@
 package com.thecookiezen.domain
 
 import com.thecookiezen.domain.Balance.EUR
-import com.thecookiezen.domain.Bank.{Address, Deposit, Error, User, Withdraw, deposit, withdraw}
+import com.thecookiezen.domain.Bank._
 import org.scalatest.{FunSuite, Matchers}
 
 class BankTransfer extends FunSuite with Matchers {
 
   test("should fail deposit with error if there is not enough balance on the account") {
-    val emptyAccount = Account.open(User("John", "Doe", 32, Address("Berlin", "Germany", "Uhlandstr", 15)))
+    val emptyAccount =
+      Account.open(User("John", "Doe", 32, Address("Berlin", "Germany", "Uhlandstr", 15)))
 
     val balanceToWithdraw = Balance(EUR, 15)
 
-    withdraw(emptyAccount)(balanceToWithdraw) shouldBe Left(s"Account balance ${emptyAccount.balance} is not sufficient for $balanceToWithdraw withdraw")
+    withdraw(emptyAccount)(balanceToWithdraw) shouldBe Left(
+      s"Account balance ${emptyAccount.balance} is not sufficient for $balanceToWithdraw withdraw")
   }
 
   test("should take deposit if there is enough balance on the account") {
-    val emptyAccount = Account.open(User("John", "Doe", 32, Address("Berlin", "Germany", "Uhlandstr", 15)))
+    val emptyAccount = Account
+      .open(User("John", "Doe", 32, Address("Berlin", "Germany", "Uhlandstr", 15)))
       .copy(balance = Balance(EUR, 25))
 
     withdraw(emptyAccount)(Balance(EUR, 15)) shouldBe Right(emptyAccount.copy(balance = Balance(EUR, 10)))
   }
 
   test("make a deposit successful deposit") {
-    val emptyAccount = Account.open(User("John", "Doe", 32, Address("Berlin", "Germany", "Uhlandstr", 15)))
+    val emptyAccount = Account
+      .open(User("John", "Doe", 32, Address("Berlin", "Germany", "Uhlandstr", 15)))
       .copy(balance = Balance(EUR, 15))
 
     deposit(emptyAccount)(Balance(EUR, 15)) shouldBe Right(emptyAccount.copy(balance = Balance(EUR, 30)))
   }
+
+  test("transfer money between accounts") {
+    val accountFrom = Account
+      .open(User("John", "Doe", 32, Address("Berlin", "Germany", "Uhlandstr", 15)))
+      .copy(balance = Balance(EUR, 15))
+
+    val accountTo =
+      Account.open(User("Alex", "Goe", 52, Address("Paris", "France", "Baguette", 2)))
+
+    transferMoney(accountFrom)(accountTo)(Balance(EUR, 15))(withdraw)(deposit) shouldBe Right(accountFrom.copy(balance = Balance(EUR, 0)),
+                                                                                              accountTo.copy(balance = Balance(EUR, 15)))
+  }
+
+  val transferMoney: Account => Account => Transfer = a1 =>
+    a2 =>
+      amount =>
+        withdraw =>
+          deposit =>
+            for {
+              res1 <- withdraw(a1)(amount)
+              res2 <- deposit(a2)(amount)
+            } yield (res1, res2)
 }
