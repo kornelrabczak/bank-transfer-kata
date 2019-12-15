b1458c35dc1070540b1e84a21bed98072bd4e538
nikom1337 <kornel.rabczak@gmail.com>
2019-12-15 13:20:20 +0100

    Moves logic from tests do the domain Bank object

--- src/main/scala/com/thecookiezen/domain/Bank.scala
+++ src/main/scala/com/thecookiezen/domain/Bank.scala
@@ -15,4 +15,15 @@ object Bank {
   type Withdraw = Account => Balance => ErrorOrAccount
 
   type Transfer = Amount => Withdraw => Deposit
+
+  val withdraw: Withdraw = account => balance => {
+    if (account.balance.amount.compare(balance.amount) >= 0)
+      Right(account.copy(balance = Balance.subtract(account.balance, balance)))
+    else
+      Left[Error, Account](s"Account balance ${account.balance} is not sufficient for $balance withdraw")
+  }
+
+  val deposit: Deposit = account => balance => {
+    Right(account.copy(balance = Balance.add(account.balance, balance)))
+  }
 }
--- src/test/scala/com/thecookiezen/domain/BankTransfer.scala
+++ src/test/scala/com/thecookiezen/domain/BankTransfer.scala
@@ -1,7 +1,7 @@
 package com.thecookiezen.domain
 
 import com.thecookiezen.domain.Balance.EUR
-import com.thecookiezen.domain.Bank.{Address, Deposit, Error, User, Withdraw}
+import com.thecookiezen.domain.Bank.{Address, Deposit, Error, User, Withdraw, deposit, withdraw}
 import org.scalatest.{FunSuite, Matchers}
 
 class BankTransfer extends FunSuite with Matchers {
@@ -27,13 +27,4 @@ class BankTransfer extends FunSuite with Matchers {
 
     deposit(emptyAccount)(Balance(EUR, 15)) shouldBe Right(emptyAccount.copy(balance = Balance(EUR, 30)))
   }
-
-  val withdraw: Withdraw = account => balance => {
-    if (account.balance.amount.compare(balance.amount) >= 0)
-      Right(account.copy(balance = Balance.subtract(account.balance, balance)))
-    else
-      Left[Error, Account](s"Account balance ${account.balance} is not sufficient for $balance withdraw")
-  }
-
-  val deposit: Deposit = account => balance => {
-    Right(account.copy(balance = Balance.add(account.balance, balance))
+}
